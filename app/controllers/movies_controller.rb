e693cff605e00740975c90aacb891a9733fe6054
Dustin Tinney <tinney@gmail.com>
2020-09-01 14:32:42 -0700

    Adding System test and passing test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,14 @@
 class MoviesController < ApplicationController
   def show
+    if params[:id] == '1'
+      title = 'Parasite' 
+      director = 'Bong Joon-ho'
+    else 
+      title = 'Titanic' 
+      director = 'James Cameron'
+    end
+
+    render :show, locals: { title: title, director: director }
   end
 end
+
