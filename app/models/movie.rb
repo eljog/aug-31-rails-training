200996e283b95c028179437af8683c59e0b5b25a
Ilana Corson <ilana.corson@gmail.com>
2020-09-02 14:23:21 -0700

    add activerecord methods and tests for movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,20 @@
 class Movie < ApplicationRecord
   # validates :title, presence: true
   validates_presence_of :title
+
+  def self.by_title
+    pluck(:title)
+  end
+
+  def self.most_to_least_fb_likes
+    order(facebook_likes:  :desc)
+  end
+
+  def self.year_by_title(title)
+    find_by_title(title)&.year 
+  end
+
+  def self.more_fb_likes_than(amount)
+    where("facebook_likes > ?", amount)
+  end
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -11,7 +11,38 @@ class MovieTest < ActiveSupport::TestCase
 
   test "movie is invalid without a title" do
     movie = Movie.create(title: "", director: "Bong Joon-ho")
-    # movie 
     refute movie.valid?
   end
+
+  test "movies titles only are returned" do
+    Movie.create(title: "Jumanji")
+    Movie.create(title: "Jumanji_2")
+    Movie.create(title: "Jumanji_3")
+
+    assert_equal ["Jumanji", "Jumanji_2", "Jumanji_3"], Movie.by_title
+  end
+
+  test "movies descending facebook_likes" do
+    movie_1 = Movie.create(title: "jumanji", facebook_likes: 6)
+    movie_2 = Movie.create(title: "Jumanji_2", facebook_likes: 2)
+    movie_3 = Movie.create(title: "jumanji 3", facebook_likes: 60)
+
+    assert_equal [movie_3, movie_1, movie_2], Movie.most_to_least_fb_likes
+  end
+
+  test "movie year by title" do
+    movie_1 = Movie.create(title: "tumanji", facebook_likes: 6, year: "2005")
+
+    assert_equal "2005", Movie.year_by_title("tumanji")
+  end
+
+  test "movies with more than x fb likes" do
+    movie_1 = Movie.create(title: "jumanji", facebook_likes: 90)
+    movie_2 = Movie.create(title: "jumanji 2", facebook_likes: 6)
+    movie_3 = Movie.create(title: "jumanji 3", facebook_likes: 44)
+    movie_4 = Movie.create(title: "jumanji 4", facebook_likes: 43)
+
+    assert_equal [movie_1, movie_3], Movie.more_fb_likes_than(43)
+  end
+
 end
