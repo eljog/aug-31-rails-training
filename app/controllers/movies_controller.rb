902cf80fd84dbaa402f2930b1a64db4bb728612d
Ilana Corson <ilana.corson@gmail.com>
2020-09-02 14:50:25 -0700

    update movie system test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,6 @@
 class MoviesController < ApplicationController
   def show
-    if params[:id] == '1'
-      title = 'Parasite' 
-      director = 'Bong Joon-ho'
-    else 
-      title = 'Titanic' 
-      director = 'James Cameron'
-    end
-
-    render :show, locals: { title: title, director: director }
+    movie = Movie.find(params[:id])
+    render locals: { movie: movie }
   end
 end
-
--- test/system/movies_system_test.rb
+++ test/system/movies_system_test.rb
@@ -3,19 +3,13 @@ require "application_system_test_case"
 class MoviesSystemTest < ApplicationSystemTestCase
   test "visiting the show" do
     attributes = { title: "Parasite", director: "Bong Joon-ho" }
-    Movie.new(attributes)
+    movie = Movie.create(attributes)
     # As a user,
     # when I visit /movies/1
-    visit("/movies/1") # visit("/movies/1")
+    visit("/movies/#{movie.id}") # visit("/movies/1")
     # I see the title of the movie "Parasite"
-    assert_text("Parasite")
+    assert_text movie.title
     # I also see the name of the director "Bong Joon-ho"
-    assert_text "Bong Joon-ho"
-  end
-
-  test "visiting for another movie" do
-    visit("/movies/2")
-    assert_text("Titanic")
-    assert_text "James Cameron"
+    assert_text movie.director
   end
 end
